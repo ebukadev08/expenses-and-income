215722175866d9fa4f3a624116eb217db3fe69ec
Ebuka <ebukaopara180@gmail.com>
2025-08-10 00:07:21 +0100

    updated

---
+++ frontend/src/config.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://expenses-backend.onrender.com/api/v1/";
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,8 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+import { API_URL } from "../config";
+const BASE_URL = API_URL;
 
 
 const GlobalContext = React.createContext()
